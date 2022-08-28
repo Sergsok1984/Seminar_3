960055a87f36ac535e8091cc8f300b7dce0602b5
Сергей Соколов <sergsok1984@gmail.com>
2022-08-28 12:05:35 +0300

    add exercise 23

---
+++ Exercise_23/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] GetCube(int n)
+{
+    int[] arr = new int [n];
+    for (int i = 1; i <= n; i++) 
+    {
+        arr [i - 1] = i * i *i;
+    }
+    return arr;
+}
+foreach (int num in GetCube(5)) 
+{
+    Console.WriteLine(num);
+}
