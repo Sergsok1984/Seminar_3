62f2948eee7fc693e4f2d276f76d793f7c39dbc2
Сергей Соколов <sergsok1984@gmail.com>
2022-08-28 11:35:51 +0300

    add exercise 21

---
+++ Exercise_21/Program.cs
@@ -0,0 +1,6 @@
+﻿double GetDistance(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
+}
+Console.WriteLine(GetDistance(7, -5, 0, 1, -1, 9));
+
